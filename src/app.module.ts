3e44aa9573aed20485463d19a2ce42bda777279d
ariefrachmansahertian <arief_rachman_sahertian@teknokrat.ac.id>
2026-04-01 05:32:00 +0700

    menambah create dan delete package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma.module';
+import { PackagesModule } from './packages/packages.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, PackagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
