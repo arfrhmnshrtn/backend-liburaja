6706200bfefdeb31d1d1a1aaeb8093e3f547fb19
ariefrachmansahertian <arief_rachman_sahertian@teknokrat.ac.id>
2026-04-01 04:49:20 +0700

    mengubah jwt screet dari hardcode menjadi mengambil di file env

--- src/auth/jwt-auth/jwt.strategy.ts
+++ src/auth/jwt-auth/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 interface JwtPayload {
   sub: string;
@@ -10,11 +11,11 @@ interface JwtPayload {
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'SECRET_KEY', // samakan dengan JWT config
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
